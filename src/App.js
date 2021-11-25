520442278897cf49351a98c097ad2d7af3e14469
Dulatto <dulat.omarov@gmail.com>
2021-11-24 22:18:37 -0500

    structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      test
-    </div>
+    <>
+      <div className='main-div'></div>
+      <div className='container-fluid'>
+        <div className='row '>
+          <div className='col-12 bg-light'>tesy</div>
+        </div>
+
+      </div>
+    </>
   );
-}
+};
 
 export default App;
