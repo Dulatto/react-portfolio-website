beb2dfc5f75300931e27ae72c05b376fe5a5285b
Dulatto <dulat.omarov@gmail.com>
2021-11-25 22:18:07 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div className='main-div'></div>
       <div className='container-fluid'>
         <div className='row '>
-          <div className='col-md-6 bg-light'>TEST</div>
-          <div className='col-md-6 bg-light'>TEST</div>
+          <div className='col-xl-2 col-lg-3  col-md-4 main-div'></div>
+          <div className='col-xl-10 col-lg-9 col-md-8 bg-light'>TEST</div>
         </div>
 
       </div>
