462dead8033c529a1a86d5f844788485a34e6270
Dulatto <dulat.omarov@gmail.com>
2021-11-25 22:09:49 -0500

    API

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 const App = () => {
   return (
-    <>
+    <div>
       <div className='main-div'></div>
       <div className='container-fluid'>
         <div className='row '>
-          <div className='col-12 bg-light'>tesy</div>
+          <div className='col-md-6 bg-light'>TEST</div>
+          <div className='col-md-6 bg-light'>TEST</div>
         </div>
 
       </div>
-    </>
+    </div>
   );
 };
 
