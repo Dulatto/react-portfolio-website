64d894bb2e80b9ff2d6a8497de13d6c3a51d3850
Dulatto <dulat.omarov@gmail.com>
2021-11-26 21:49:15 -0500

    menu style

--- src/Views/Menu.js
+++ src/Views/Menu.js
@@ -3,10 +3,10 @@ import menu from '../data';
 
 function Menu(props) {
     return (
-        <ul className='menu-style'>
+        <ul className='menu-style '>
             {menu.map(item => {
                 return (
-                    <li key={item.id} className='text-light'>
+                    <li key={item.id} className='text-light d-flex justify-content-center'>
                         {item.name}
                     </li>
                 )
