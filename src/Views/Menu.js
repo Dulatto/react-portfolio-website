110531528d617cd5d9956e0086a9016dc5408613
Dulatto <dulat.omarov@gmail.com>
2021-11-27 21:17:24 -0500

    Router

--- src/Views/Menu.js
+++ src/Views/Menu.js
@@ -1,22 +1,49 @@
 import React from 'react';
-//import menu from '../data';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function Menu(props) {
     return (
-        <ul className='menu-style '>
-            <li className='text-light d-flex justify-content-center '>
-                HOME
-            </li>
-            <li className='text-light d-flex justify-content-center '>
-                ABOUT
-            </li>
-            <li className='text-light d-flex justify-content-center '>
-                PORTFOLIO
-            </li>
-            <li className='text-light d-flex justify-content-center '>
-                CONTACT
-            </li>
-        </ul>
+
+        <Router>
+            <div>
+                <ul className='menu-style '>
+                    <li className='text-light d-flex justify-content-center '>
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li className='text-light d-flex justify-content-center '>
+                        <Link to="/about">ABOUT</Link>
+                    </li>
+                    <li className='text-light d-flex justify-content-center '>
+                        <Link to="/portfolio">PORTFOLIO</Link>
+                    </li>
+                    <li className='text-light d-flex justify-content-center '>
+                        <Link to="/contact">CONTACT</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/portfolio">
+                        <Portfolio />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
