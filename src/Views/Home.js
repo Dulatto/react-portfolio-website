865e2208188f02910535611d9dd5d795b8d5ea83
Dulatto <dulat.omarov@gmail.com>
2021-11-29 21:53:02 -0500

    font

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(props) {
     return (
         <div className='text-white home-style'>
-            <h1 className='h1-styled'>Hi, <br /> I'm <span className='name-color'>D</span>ulat,</h1>
+            <h1 className='h1-styled'><span>H</span><span>i</span><span>,</span> <br /> <span>I</span><span>'</span><span>m</span> <span className='name-color'>D</span>ulat,</h1>
             <h1 className='h1-styled'>web developer</h1>
             <p>Frontend Web Developer</p>
             <button type="button" class="btn btn-outline-primary">Contact Me</button>
