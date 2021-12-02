8dbda7fc28a870997fccac3808cbb5679257fdec
Dulatto <dulat.omarov@gmail.com>
2021-12-01 21:53:40 -0500

    contact

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -4,10 +4,18 @@ function Contact(props) {
     return (
         <div className='contact-style'>
             <div className='row'>
-                <div className='col-md-6'></div>
+                <div className='col-md-6'>
+                    <h1 className='mt-5 ms-4'>
+                        <span className='hvr-wobble-vertical'>C</span>
+                        <span className='hvr-wobble-vertical'>o</span>
+                        <span className='hvr-wobble-vertical'>n</span>
+                        <span className='hvr-wobble-vertical'>t</span>
+                        <span className='hvr-wobble-vertical'>a</span>
+                        <span className='hvr-wobble-vertical'>c</span>
+                        <span className='hvr-wobble-vertical'>t</span>    <span className='hvr-wobble-vertical'>m</span><span className='hvr-wobble-vertical'>e</span></h1>
+                </div>
                 <div className='col-md-6'></div>
             </div>
-            Contact
         </div>
     );
 }
