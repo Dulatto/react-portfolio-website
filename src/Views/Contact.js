2a36b4a3275d7c74a7237df7e4a4a2b015d718f4
Dulatto <dulat.omarov@gmail.com>
2021-12-01 21:42:25 -0500

    contact

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -3,6 +3,10 @@ import React from 'react';
 function Contact(props) {
     return (
         <div className='contact-style'>
+            <div className='row'>
+                <div className='col-md-6'></div>
+                <div className='col-md-6'></div>
+            </div>
             Contact
         </div>
     );
