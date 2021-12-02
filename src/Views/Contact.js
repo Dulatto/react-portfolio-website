7140c96f27a27a830ced7274c6f48349e0f39eba
Dulatto <dulat.omarov@gmail.com>
2021-12-01 22:38:13 -0500

    contact

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -13,6 +13,31 @@ function Contact(props) {
                         <span className='hvr-wobble-vertical'>a</span>
                         <span className='hvr-wobble-vertical'>c</span>
                         <span className='hvr-wobble-vertical'>t</span>    <span className='hvr-wobble-vertical'>m</span><span className='hvr-wobble-vertical'>e</span></h1>
+                    <p className='my-5 ms-4'>I’m interested in freelance opportunities – especially ambitious or large projects.
+                        However, if you have other request or question, don’t hesitate to use the form.</p>
+                    <div className='row ms-4'>
+                        <div className='col-6'>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Name" aria-describedby="inputGroup-sizing-default"></input>
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Email" aria-describedby="inputGroup-sizing-default"></input>
+                            </div>
+                        </div>
+                        <div className='col-12'>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Subject" aria-describedby="inputGroup-sizing-default"></input>
+                            </div>
+                        </div>
+                        <div className='col-12'>
+                            <div className="form-floating">
+                                <textarea className="form-control" style={{ height: 100 + 'px' }}></textarea>
+                                <label >Message</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='col-md-6'></div>
             </div>
