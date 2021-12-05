82cef7285cb8cbadbb5d6bcec4d0fbbf98319eea
Dulatto <dulat.omarov@gmail.com>
2021-12-05 16:32:29 -0500

    gallery

--- src/Views/Portfolio.js
+++ src/Views/Portfolio.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const images = [
+    { id: 1, text: 'Dating', imageId: 1, description: 'HTML, JavaScript', type: 'JS' },
+    { id: 2, text: 'Dating', imageId: 2, description: 'HTML, JavaScript', type: 'JS' },
+    { id: 3, text: 'Dating', imageId: 3, description: 'HTML, JavaScript', type: 'JS' }
+]
+
 function Portfolio(props) {
     return (
         <div className='portfolio-style'>
-            Portfolio
+            <div className='row'>
+                <div className='col-md-4'></div>
+                <div className='col-md-4'></div>
+                <div className='col-md-4'></div>
+            </div>
         </div>
     );
 }
