cbe855a6e3345ff201c6cedf932506903036e92b
Dulatto <dulat.omarov@gmail.com>
2021-12-05 17:05:52 -0500

    portfolio

--- src/Views/Portfolio.js
+++ src/Views/Portfolio.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const images = [
-    { id: 1, text: 'Dating', imageId: 1, description: 'HTML, JavaScript', type: 'JS' },
-    { id: 2, text: 'Dating', imageId: 2, description: 'HTML, JavaScript', type: 'JS' },
-    { id: 3, text: 'Dating', imageId: 3, description: 'HTML, JavaScript', type: 'JS' }
+    { id: 1, text: 'Dating', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 2, text: 'Restaurant', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 3, text: 'Dating', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 4, text: 'Medical', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 5, text: 'Book', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 6, text: 'Pet', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 7, text: 'Cafe', description: 'HTML, JavaScript', type: 'JS' },
+    { id: 8, text: 'Weather', description: 'HTML, JavaScript', type: 'React' },
+    { id: 9, text: 'Todo', description: 'HTML, JavaScript', type: 'React' },
+    { id: 10, text: 'Tracker', description: 'HTML, JavaScript', type: 'React' },
+    { id: 11, text: 'Portfolio', description: 'HTML, JavaScript', type: 'React' },
 ]
 
 function Portfolio(props) {
