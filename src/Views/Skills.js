13927958a8dbebf6cd3c2c877f17522cae993a58
Dulatto <dulat.omarov@gmail.com>
2021-12-12 22:25:49 -0500

    form

--- src/Views/Skills.js
+++ src/Views/Skills.js
@@ -5,19 +5,32 @@ const Skills = () => {
         <div className='skills-style'>
             <div className='row'>
                 <div className='col-md-6 '>
-                    <div className='contact-cont'>
+                    <div className='skills-cont'>
                         <div className='my-5'>&nbsp;</div>
                         <h1 className='mt-5 ms-4'>
-                            <span className='hvr-wobble-vertical'>C</span>
-                            <span className='hvr-wobble-vertical'>o</span>
+                            <span className='hvr-wobble-vertical'>S</span>
+                            <span className='hvr-wobble-vertical'>k</span>
+                            <span className='hvr-wobble-vertical'>i</span>
+                            <span className='hvr-wobble-vertical'>l</span>
+                            <span className='hvr-wobble-vertical'>l</span>
+                            <span className='hvr-wobble-vertical'>s</span>
+                            <br />  <span className='hvr-wobble-vertical'>&</span>    <span className='hvr-wobble-vertical'>E</span>
+                            <span className='hvr-wobble-vertical'>x</span>
+                            <span className='hvr-wobble-vertical'>p</span>
+                            <span className='hvr-wobble-vertical'>e</span>
+                            <span className='hvr-wobble-vertical'>r</span>
+                            <span className='hvr-wobble-vertical'>i</span>
+                            <span className='hvr-wobble-vertical'>e</span>
                             <span className='hvr-wobble-vertical'>n</span>
-                            <span className='hvr-wobble-vertical'>t</span>
-                            <span className='hvr-wobble-vertical'>a</span>
                             <span className='hvr-wobble-vertical'>c</span>
-                            <span className='hvr-wobble-vertical'>t</span>    <span className='hvr-wobble-vertical'>m</span><span className='hvr-wobble-vertical'>e</span></h1>
-                        <p className='my-5 ms-4'>I’m interested in freelance opportunities – especially ambitious or large projects.
-                            However, if you have other request or question, don’t hesitate to use the form.</p>
-
+                            <span className='hvr-wobble-vertical'>e</span></h1>
+                        <p className='my-5 ms-4'>Since beginning my journey as a freelance developer nearly 10 years ago,
+                            I’ve done remote work for agencies, consulted for startups, and collaborated with talented people
+                            to create web products for both business and consumer use.</p>
+                        <p>I create successful responsive websites that are fast, easy to use, and built with best practices.
+                            The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium web apps,
+                            custom plugins, features, animations, and coding interactive layouts.</p>
+                        <p>Visit my <a href="https://www.linkedin.com/in/dulat-omarov/">LinkedIn </a>profile for more details or just <a href="http://localhost:3001/contact">contact </a>me.</p>
                     </div>
                 </div>
                 <div className='col-md-6'></div>
